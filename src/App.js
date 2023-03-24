a0a728c9ce79a00e5c9a7fdae29b36c437f9b710
Jibran-Sajid <jibransajid24@gmail.com>
2023-03-25 01:32:03 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,25 @@
 import React from 'react';
-import ChangePassword from './change-password/ChangePassword';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
-import ForgetPassword from './ForgetPassword.js/ForgetPassword';
-import Login from './login/Login';
 import UpdateProfile from './update-profile/UpdateProfile';
 
 function App() {
   return (
-    <React.Fragment>
 
-      {/* <Login/> */}
-      {/* <ForgetPassword/> */}
-            
+    <React.Fragment>
 
       <Navbar />
 
       <div class="container">
-
         <div class="row mt-md-5">
 
-            <ChangePassword />
             <UpdateProfile/>
             
         </div>
-
       </div>
 
       <Footer />
+
     </React.Fragment>
   );
 }
